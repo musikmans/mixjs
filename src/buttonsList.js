10fe10cfa4e1076f11dae6294a8693e8d3b4e58e
François Tremblay <francois@ftremblay.net>
2019-01-29 19:27:12 -0800

    Revising Structure and adding buttons properties array

---
+++ src/buttonsList.js
@@ -0,0 +1,177 @@
+export default [
+    {
+        class: "vol-1",
+        src: "Assets/knob_left-02.svg",
+        alt: "Volume on left side"
+    },
+    {
+        class: "fx-delay-1",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx Delay on left side"
+    },
+    {
+        class: "fx-reverb-1",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx Reverb on left side"
+    },
+    {
+        class: "fx-pan-1",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx Pan on left side"
+    },
+    {
+        class: "fx-lpf-1",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx Low-Pass Filter on left side"
+    },
+    {
+        class: "fx-hpf-1",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx High-Pass Filter on left side"
+    },
+    {
+        class: "fader-pitch-left",
+        src: "Assets/fader.svg",
+        alt: "Pitch fader on left side"
+    },
+    {
+        class: "vinyl-left",
+        src: "Assets/vinyl.svg",
+        alt: "Vinyl left"
+    },
+    {
+        class: "eject-left",
+        src: "Assets/eject_inactive.svg",
+        alt: "Eject left side"
+    },
+    {
+        class: "rew-left",
+        src: "Assets/rew_inactive.svg",
+        alt: "Rewind left side"
+    },
+    {
+        class: "play-left",
+        src: "Assets/play_inactive.svg",
+        alt: "Play left side"
+    },
+    {
+        class: "forward-left",
+        src: "Assets/forward_inactive.svg",
+        alt: "Forward left side"
+    },
+    {
+        class: "stop-left",
+        src: "Assets/stop_inactive.svg",
+        alt: "Stop left side"
+    },
+    {
+        class: "loop-one-left",
+        src: "Assets/one_bar_inactive.svg",
+        alt: "Loop one bar left side"
+    },
+    {
+        class: "loop-four-left",
+        src: "Assets/four_bars_inactive.svg",
+        alt: "Loop four bar left side"
+    },
+    {
+        class: "loop-eight-left",
+        src: "Assets/eight_bars_inactive.svg",
+        alt: "Loop eight bar left side"
+    },
+    {
+        class: "loop-sixteen-left",
+        src: "Assets/sixteen_bars_inactive.svg",
+        alt: "Loop sixteen bar left side"
+    },
+    {
+        class: "vol-2",
+        src: "Assets/knob_left-02.svg",
+        alt: "Volume on right side"
+    },
+    {
+        class: "fx-delay-2",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx Delay on right side"
+    },
+    {
+        class: "fx-reverb-2",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx Reverb on right side"
+    },
+    {
+        class: "fx-pan-2",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx Pan on right side"
+    },
+    {
+        class: "fx-lpf-2",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx Low-Pass Filter on right side"
+    },
+    {
+        class: "fx-hpf-2",
+        src: "Assets/knob_left-02.svg",
+        alt: "Fx High-Pass Filter on right side"
+    },
+    {
+        class: "fader-pitch-right",
+        src: "Assets/fader.svg",
+        alt: "Pitch fader on right side"
+    },
+    {
+        class: "vinyl-right",
+        src: "Assets/vinyl.svg",
+        alt: "Vinyl right"
+    },
+    {
+        class: "eject-right",
+        src: "Assets/eject_inactive.svg",
+        alt: "Eject right side"
+    },
+    {
+        class: "rew-right",
+        src: "Assets/rew_inactive.svg",
+        alt: "Rewind right side"
+    },
+    {
+        class: "play-right",
+        src: "Assets/play_inactive.svg",
+        alt: "Play right side"
+    },
+    {
+        class: "forward-right",
+        src: "Assets/forward_inactive.svg",
+        alt: "Forward right side"
+    },
+    {
+        class: "stop-right",
+        src: "Assets/stop_inactive.svg",
+        alt: "Stop right side"
+    },
+    {
+        class: "loop-one-right",
+        src: "Assets/one_bar_inactive.svg",
+        alt: "Loop one bar right side"
+    },
+    {
+        class: "loop-four-right",
+        src: "Assets/four_bars_inactive.svg",
+        alt: "Loop four bar right side"
+    },
+    {
+        class: "loop-eight-right",
+        src: "Assets/eight_bars_inactive.svg",
+        alt: "Loop eight bar right side"
+    },
+    {
+        class: "loop-sixteen-right",
+        src: "Assets/sixteen_bars_inactive.svg",
+        alt: "Loop sixteen bar right side"
+    },
+    {
+        class: "x-fader",
+        src: "Assets/x_fader.svg",
+        alt: "Crossfader"
+    },
+  ];
