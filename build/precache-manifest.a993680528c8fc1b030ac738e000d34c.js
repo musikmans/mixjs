8f39537dad31197d0fc3af1e1dccd4ca89d8fd4c
François Tremblay <francois@ftremblay.net>
2019-03-05 14:52:05 -0800

    build production app

---
+++ build/precache-manifest.a993680528c8fc1b030ac738e000d34c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91fdca84ef95a7480949",
+    "url": "/static/js/main.91fdca84.chunk.js"
+  },
+  {
+    "revision": "3a6e11a93d9188b6c247",
+    "url": "/static/js/1.3a6e11a9.chunk.js"
+  },
+  {
+    "revision": "91fdca84ef95a7480949",
+    "url": "/static/css/main.165f1f7c.chunk.css"
+  },
+  {
+    "revision": "8a5f7a731c91b0adb6f3d60e70f2fc6c",
+    "url": "/index.html"
+  }
+];
